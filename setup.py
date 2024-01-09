50458cd9310b9998c6d27631a65e251d5455549e
Owen Rubenis <103213187+OwenRube7@users.noreply.github.com>
2024-01-09 11:37:03 +0200

    Update setup.py and added new files for Climate Pylot

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Climate Pylot',  # Required
-    version='1.0.0',  # Required
-    description='A brief description of your package',  # Optional
-    long_description='A longer description of your package',  # Optional
-    url='https://github.com/OwenRube7/climate-pylot',  # Optional
-    author='Owen Rubenis',  # Optional
-    classifiers=[  # Optional
+    name='Climate Pylot',
+    version='1.0.0',
+    description='A brief description of your package',
+    long_description='A longer description of your package',
+    url='https://github.com/OwenRube7/climate-pylot',
+    author='Owen Rubenis',
+    classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.12.1',
     ],
-    keywords='sample setuptools development',  # Optional
+    keywords='sample setuptools development',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    entry_points={  # Optional
+    entry_points={
         'console_scripts': [
             'sample=sample:main',
         ],
     },
-    project_urls={  # Optional
+    project_urls={
         'Bug Reports': 'https://github.com/OwenRube7/climate-pylot/issues',
         'Source': 'https://github.com/OwenRube7/climate-pylot',
     },
--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,32 @@
-"""
 import tkinter
 
 root = tkinter.Tk()
 
-root.configure(bg='black')
-
-label = tkinter.Label(root, text="Welcome to Climate Pylot\n Please select target region...", bg='black', fg='white')
+label = tkinter.Label(root, text="Welcome to Climate Pylot\n Please select target region...")
 label.pack()
 
 menu = tkinter.Menu(root)
 root.config(menu=menu)
 
+file_menu = tkinter.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+
 region_menu = tkinter.Menu(menu)
 menu.add_cascade(label="Regions", menu=region_menu)
 
-region_menu.add_command(label="North America", command=lambda: open_folder("north_america))
+def open_folder(region):
+    import os
+    import subprocess
+    folder_path = os.path.join("./src", region)
+    init_file_path = os.path.join(folder_path, "__init__.py")
+    subprocess.run(["python", init_file_path])
+
+region_menu.add_command(label="North America", command=lambda: open_folder("north_america"))
 region_menu.add_command(label="Europe", command=lambda: open_folder("europe"))
 region_menu.add_command(label="South America", command=lambda: open_folder("south_america"))
 region_menu.add_command(label="Asia", command=lambda: open_folder("asia"))
 region_menu.add_command(label="Africa", command=lambda: open_folder("africa"))
 region_menu.add_command(label="Oceania", command=lambda: open_folder("oceania"))
-region_menu.add_command(label="Antarctica", command=lambda: open_folder("antarctica))
+region_menu.add_command(label="Antarctica", command=lambda: open_folder("antarctica"))
 
 root.mainloop()
-"""
